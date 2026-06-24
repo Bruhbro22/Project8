82b4350e12dd2c4b689d225488e3967413bb76d5
Bruhbro22 <donmadhu411@gmail.com>
2026-06-24 10:54:23 -0500

    Add files via upload

---
+++ Project8.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a number here: "))
+b=int(input("Enter a number here: "))
+c=int(input("Enter a number here: "))
+print("Before swaping:,a=(a),b=(b),c=(c)")
+a,b,c=c,a,b
+print("After swapping:,a=c b=a c=b which a=",a,"b=",b,"c=",c)
